668c3894e678fc804b25025b6ce2fcf24a3d40a8
Frank Hermann <alphargon@gmail.com>
2022-03-16 14:22:03 +0100

    anti cyclic imp

--- brute_code/code.c
+++ brute_code/code.c
@@ -3,10 +3,27 @@
 
 using namespace std;
 
-static const unsigned int N = N_VAL, M = M_VAL, maxL = 60; // we assume M < N
+#ifndef N_VAL
+#error "define N_VAL"
+#endif
+#ifndef M_VAL
+#error "define M_VAL"
+#endif
+#ifndef MAX_L_VAL
+#define "define MAX_L_VAL"
+#endif
+static const unsigned int N = N_VAL, M = M_VAL, maxL = MAX_L_VAL; // we assume M < N
+#ifdef MIN_L_VAL
+static const unsigned int minL = MIN_L_VAL;
+#else
 static const unsigned int minL = (max(N, M + 1) + 1)/2*2;
+#endif
 
+#ifndef BIT_128
+typedef unsigned __int128 code_t;
+#else
 typedef uint64_t code_t;
+#endif
 
 /*
  * print code in +- format
@@ -74,19 +91,68 @@ __attribute__((optimize("unroll-loops"), always_inline)) inline void check(unsig
 }
 
 int main(){
+#ifdef IMPL_ENUM
+    cout << "running enumerate impl" << endl;
     // loop over bitvector length
     for(unsigned int L = minL; L <= maxL; L += 2){
         cout << "L: " << L << endl;
-        auto start = chrono::system_clock::now();
         // note that this parallelized loop acually checks only a quarter of the codes
         #pragma omp parallel for
         for(code_t c = code_t(1) << (L - 1); code_t(c) < (code_t(1) << L); c += 2){
             check(L, c);
         }
-        auto end = chrono::system_clock::now();
-        int elapsed = chrono::duration_cast<chrono::nanoseconds>(end - start).count();
-        if(elapsed > 0){
-            cout << "codes per sec " << 500*((code_t(1) << L) - (code_t(1) << (L - 1)))/elapsed << "e6" << endl;
+    }
+#else
+    typedef unsigned int uint;
+    typedef unsigned int run_len_t;
+
+    cout << "running anti-cyclic impl" << endl;
+    for(uint L = minL; L <= maxL; L += 2){
+        cout << "L: " << L << endl;
+        code_t alternating = 0;
+        for(uint i = 0; i < sizeof(code_t)*8; i += 2){
+            alternating += (code_t(1) << i);
+        }
+        #pragma omp parallel for num_threads(16)
+        for(uint leading = L - 1; leading >= 2; leading--){ // leading = 1 not supported
+            run_len_t sol[L - 2];
+            uint c = L - leading;
+            code_t b = (code_t(1) << (leading - 1)) - 1; // start with leading sequence one bit shorter
+            for(uint i = 0; i < L - leading; i++){
+                sol[i] = 1;
+            }
+            uint runs = 0;
+            b = b << (c + 1);
+            c += 1;
+            #pragma omp critical 
+            {
+                cout << "leading " << leading << endl;
+            }
+            while(1){
+                b = ((b >> c) << c) | (alternating >> (sizeof(code_t)*8 - 1 - c + (runs&1)));
+                runs += c - 2; // all added runs should have length 1
+                if((runs&1) == 0){
+                    check(L, b);
+                }
+                if(runs == 0){
+                    break;
+                }
+                c = sol[runs];
+                sol[runs] = 1;
+                code_t prev = sol[runs - 1];
+                while(prev == leading){
+                    runs--;
+                    if(runs == 0){
+                        goto out;
+                    }
+                    c += prev;
+                    sol[runs] = 1; // prev = 1
+                    prev = sol[runs - 1];
+                }
+                sol[runs - 1] = prev + 1; // prev += 1
+            }
+            out:;
         }
     }
+#endif
 }
