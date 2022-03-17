871b9f9ce05c42861659a3f1077ca0888427eb2b
Frank Hermann <alphargon@gmail.com>
2022-03-17 19:41:49 +0100

    add plots

--- cyclic_counter_prototypes.py
+++ cyclic_counter_prototypes.py
@@ -33,21 +33,43 @@ for L in range(3, 25):
 x.append(34)
 y.append(2**(34 - 2)/358728827)
 x.append(36)
+#                    1687422392
 y.append(2**(36 - 2)/1356530952)
 
 #%%
 
-# no parallel 157 (static schedule 50, def schedule 44, 1024 schedule 36, 4096: 48)
-
-plt.scatter([36, 34, 32], [50.76/10, 7.81/1.95, 0.804/0.77], label="notebook, 64 bit, single core, exhaustive, inner exact loop")
-plt.plot(x, y, label="max speedup", c="black")
+# runs with N=6, M=8
+plt.scatter([32, 34, 36], [6.5/0.612, 20.45/1.91, 105/9], label="single core, exhaustive")
+plt.scatter([32, 34, 36], [1.194/0.134, 3.417/0.368, 18.34/1.668], label="multi core, exhaustive")
+plt.plot(x, y, label="theoretical maximum", c="black")
 plt.grid()
 plt.legend()
-plt.title("speedup")
+plt.title("speedup on server, 128bit")
 plt.grid()
 plt.show()
 
 #%%
+
+# factor three penalty for using 128bit vector instead of 64bit
+plt.scatter([32, 34, 36], [1.194, 3.417, 18.34], label="exhaustive, 128bit, enum")
+plt.scatter([32, 34, 36], [0.134, 0.368, 1.668], label="exhaustive, 128bit, loops")
+plt.scatter([48], [8*60 + 55], label="N=8, M=6, 128bit, enum")
+plt.scatter([48], [5*60 + 8.4], label="N=8, M=6, 128bit, loops")
+plt.scatter([48], [1*60 + 44], label="N=8, M=6, 64bit, loops")
+plt.scatter([40], [0.189], label="N=7, M=5, 128bit, enum")
+plt.scatter([40], [0.514], label="N=6, M=8, 128bit, loops")
+plt.yscale("log")
+plt.axhline(3600*24, label="1 day")
+plt.grid()
+plt.xlim(31, 60)
+plt.ylim(0.1, 10e5)
+plt.title("runtime, multi-core")
+plt.legend(loc="upper left")
+plt.show()
+
+
+#%%
+
 
 L = 10
 sols = solve_recursive(L)
@@ -118,9 +140,6 @@ for brute_bits in range(start, end + 1, 2):
     sols.append(("brute", f"{brute_bits:02b}"))
 sols.append(("last", f"{(alternating >> (63 - L)):b}"))
 assert set(sols_check) == set(t[-1] for t in sols)
-
-
-# 11110XXX10XXX10
 
 #%%
 
