b67f24af6715f7e65934535264e44bbe497c07e9
Frank Hermann <alphargon@gmail.com>
2022-04-07 01:47:00 +0200

    add new upper limit for code solution

--- solution_plot.py
+++ solution_plot.py
@@ -16,19 +16,23 @@ def check_code(c, k, h, Delta):
              cc[L + j + Delta + h - k]*cc[L + j + h - k])
             for j in range(L))
     if r != 0:
-        print("code error:", k, h, Delta, r)
+        print("code error:", h, Delta, ":", k)
+    return r
 
 
 def full_check(c, N, M):
+    err = 0
     for k in range(N):
         for h in range(N):
             if k != h:
                 for Delta in range(1, M+1):
-                    check_code(c, k, h, Delta)
+                    err += check_code(c, k, h, Delta)
+    print(err)
 
 sols = []
 f = open("brute_code/solution_summary")
 L_vals = set()
+golden_code = []
 for line in f:
     L = int(line.split(",")[1])
     N = int(line.split(",")[2])
@@ -38,6 +42,7 @@ for line in f:
     full_check(code, N, M-1)
     assert len(code) == L
     if N==8 and M==6:
+        golden_code = code
         print(code)
     cur = code[0]
     res = [0]
@@ -49,6 +54,15 @@ for line in f:
             res.append(1)
     print(f"N: {N:2d}, M: {M:2d} L: {L:2d}, runs: {len(res):2d}")
     sols.append((N, M, code))
+
+sols.append((9, 7, [], 58))
+sols.append((9, 6, [], 48))
+sols.append((9, 5, [], 48))
+sols.append((9, 4, [], 48))
+sols.append((10, 6, [], 48))
+sols.append((10, 5, [], 48))
+sols.append((10, 4, [], 48))
+
 max_N = max(t[0] for t in sols)
 max_M = max(t[1] for t in sols)
 res = np.zeros((max_M, max_N-1), dtype=np.int32)
@@ -56,6 +70,8 @@ for N in range(2, max_N + 1):
     for M in range(1, max_M + 1):
         try:
             L = len(next(t for t in sols if t[0] == N and t[1] == M)[2])
+            if L == 0:
+                L = -1
         except StopIteration:
             L = -1
         res[M-1, N-2] = L
@@ -75,8 +91,13 @@ for N in range(2, max_N+1):
     for M in range(1, max_M+1):
         if res[M-1, N-2] != -1:
             plt.text(N-2, M-1, str(res[M-1, N-2]), va='center', ha='center')
-        elif N >= 8:
-            plt.text(N-2, M-1, ">48", va='center', ha='center')
+            continue
+        try:
+            code_inf = next(t for t in sols if t[0] == N and t[1] == M)
+            if len(code_inf) > 3:
+                plt.text(N-2, M-1, f">{code_inf[3]}", va='center', ha='center')
+        except StopIteration:
+            pass
 plt.clim(1)
 plt.xlabel("N")
 plt.ylabel("M")
